b3a4b868b1be0e1ce4438f54ae5d528d94c29306
lateefshow <lateefshooo@gmail.com>
2025-05-11 23:03:19 +0100

    Add files via upload

---
+++ Tenery Operator/script.js
@@ -0,0 +1,5 @@
+
+// --- Ternary Operator ---
+let age = 18;
+let message = (age >= 18) ? "You are an adult." : "You are a minor.";
+console.log("Ternary result:", message);
---
+++ Unary Operator/script.js
@@ -0,0 +1,10 @@
+// JavaScript Unary Operators Example
+
+// --- Unary Operator ---
+let a = 5;
+console.log("Unary minus (-a):", -a);        // -5 (negation)
+console.log("Unary plus (+a):", +a);         // 5 (ensures it's a number)
+
+let isAvailable = false;
+console.log("Logical NOT (!isAvailable):", !isAvailable); // true (inverts the boolean)
+
